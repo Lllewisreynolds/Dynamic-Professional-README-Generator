76b5183525109110c5bf695af6f36248897835eb
Lllewisreynolds <lsreynolds108@gmail.com>
2023-11-30 11:35:42 +0000

    init function finalised and now creates Markdown file. Licence badges and link sourced and made accessible within render functions in generateMarkdown.js. Then called within generateMarkdown

--- index.js
+++ index.js
@@ -74,44 +74,20 @@ const questions = [
       },
 
       { //A further type of prompt used - checkbox to allow license choice from common options
-        type: 'checkbox',
+        type: 'list',
         // 'lienCe' because I'm British!
         name: 'licence',
         message: 'Please select a commonly-used licence from the options provided below.',
-        choices: ['Academic Free License v3.0', 'Apache license 2.0',
-        'Artistic license 2.0',	
+        choices: [
+        'Apache license 2.0',
         'Boost Software License 1.0',
-        'BSD 2-clause "Simplified" license',	
-        'BSD 3-clause "New" or "Revised" license',	
-        'BSD 3-clause Clear license',
-        'BSD 4-clause "Original" or "Old" license',	
-        'BSD Zero-Clause license',
-        'Creative Commons license family',	
-        'Creative Commons Zero v1.0 Universal',
-        'Creative Commons Attribution 4.0',	
-        'Creative Commons Attribution ShareAlike 4.0',
-        'Do What The F*ck You Want To Public License',	
-        'Educational Community License v2.0',
-        'Eclipse Public License 1.0',	
-        'Eclipse Public License 2.0',	
-        'European Union Public License 1.1',
         'GNU Affero General Public License v3.0',
-        'GNU General Public License family',
-        'GNU General Public License v2.0',	
         'GNU General Public License v3.0',
-        'GNU Lesser General Public License family',	
-        'GNU Lesser General Public License v2.1',
         'GNU Lesser General Public License v3.0',
-       'LaTeX Project Public License v1.3c',
-        'Microsoft Public License',
-        'MIT	MIT',
+        'MIT License',
         'Mozilla Public License 2.0',
-        'Open Software License 3.0',
-        'PostgreSQL License	PostgreSQL',
-        'SIL Open Font License 1.1',
-        'University of Illinois/NCSA Open Source License',
         'The Unlicense',
-        'zLib License'],
+        ],
 
         validate: licenceSelected => {
           if (licenceSelected) {
@@ -215,7 +191,8 @@ function init() {
 
 return inquirer.prompt(questions)
 .then(displayReponse => {
-return displayReponse;
+let info = generateMarkdown(displayReponse);
+writeToFile('./Generated_README/README1.md', info);
     })
 }
 
