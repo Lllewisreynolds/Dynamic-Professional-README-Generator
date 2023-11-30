02e12a30cb968f607f352a2624c0320fb5c4d1e1
Lllewisreynolds <lsreynolds108@gmail.com>
2023-11-30 13:39:49 +0000

    Rendering links function now displays URL and title of selected Licence

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,25 +4,25 @@ function renderLicenseBadge(licence) {
   if (licence === 'Apache license 2.0') {
    return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)' 
   } 
-  if (licence === 'Boost Software License 1.0') {
+  else if (licence === 'Boost Software License 1.0') {
     return '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)' 
    } 
-   if (licence === 'GNU Affero General Public License v3.0') {
+   else if (licence === 'GNU Affero General Public License v3.0') {
     return '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)' 
    } 
-   if (licence === 'GNU General Public License v3.0') {
+   else if (licence === 'GNU General Public License v3.0') {
     return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)' 
    } 
-   if (licence === 'GNU Lesser General Public License v3.0') {
+   else if (licence === 'GNU Lesser General Public License v3.0') {
     return '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)' 
    } 
-   if (licence === 'MIT License') {
+   else if (licence === 'MIT License') {
     return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)' 
    } 
-   if (licence === 'Mozilla Public License 2.0') {
+   else if (licence === 'Mozilla Public License 2.0') {
     return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)' 
    } 
-   if (licence === 'The Unlicense') {
+   else if (licence === 'The Unlicense') {
     return '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)' 
    } 
   else {
@@ -34,28 +34,28 @@ function renderLicenseBadge(licence) {
 // If there is no license, return an empty string
 function renderLicenseLink(licence) {
   if (licence === 'Apache license 2.0') {
-    return '[License Link](https://opensource.org/licenses/Apache-2.0)' 
+    return '[Link to licence](https://opensource.org/licenses/Apache-2.0)' 
    } 
    if (licence === 'Boost Software License 1.0') {
-     return '[License Link](https://www.boost.org/LICENSE_1_0.txt)' 
+     return '[Link to licence](https://www.boost.org/LICENSE_1_0.txt)' 
     } 
     if (licence === 'GNU Affero General Public License v3.0') {
-     return '[License Link](https://www.gnu.org/licenses/agpl-3.0)' 
+     return '[Link to licence](https://www.gnu.org/licenses/agpl-3.0)' 
     } 
     if (licence === 'GNU General Public License v3.0') {
-     return '[License Link](https://www.gnu.org/licenses/gpl-3.0)' 
+     return '[Link to licence](https://www.gnu.org/licenses/gpl-3.0)' 
     } 
     if (licence === 'GNU Lesser General Public License v3.0') {
-     return '[License Link](https://www.gnu.org/licenses/lgpl-3.0)' 
+     return '[Link to licence](https://www.gnu.org/licenses/lgpl-3.0)' 
     } 
     if (licence === 'MIT License') {
-     return '[License Link](https://opensource.org/licenses/MIT)' 
+      return '[Link to licence](https://opensource.org/licenses/MIT)';
     } 
     if (licence === 'Mozilla Public License 2.0') {
-     return '[License Link](https://opensource.org/licenses/MPL-2.0)' 
+     return '[Link to licence](https://opensource.org/licenses/MPL-2.0)' 
     } 
     if (licence === 'The Unlicense') {
-     return '[License Link](http://unlicense.org/)'
+     return '[Link to licence](http://unlicense.org/)'
     } 
    else {
      return '';
@@ -189,7 +189,10 @@ let tableOfContents =
 ## License
 
       
-  This application has the following licence: ${data.licence}
+  This application has the following licence:
+
+
+  ${data.licence}
   
   ${renderLicenseLink(data.licence)}
 
